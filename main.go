59143610dcb5757982c07fc6145501019d2e23da
turtle85917 <siu85917@gmail.com>
2022-11-13 16:22:13 +0900

    Accepting feedback from code reviewers

--- main.go
+++ main.go
@@ -12,70 +12,65 @@ import (
 )
 
 const (
-	screenSizeX = TileSize * (Width + 2)
-	screenSizeY = TileSize * (Height + 2)
-	Width       = 15
-	Height      = 12
-	TileSize    = 50
+	screenSizeX = tileSize * (width + 2)
+	screenSizeY = tileSize * (height + 2)
+	width       = 15
+	height      = 12
+	tileSize    = 50
 )
 
+var colors = [...]color.RGBA{red, orange, yellow, green, blue, purple, brown}
 var (
-	White  = color.RGBA{255, 255, 255, 255}
-	Tile   = color.RGBA{77, 77, 77, 255}
-	NGoal  = color.RGBA{255, 150, 150, 255}
-	YGoal  = color.RGBA{65, 130, 35, 255}
-	Player = color.RGBA{225, 227, 167, 255}
+	white  = color.RGBA{255, 255, 255, 255}
+	tile   = color.RGBA{77, 77, 77, 255}
+	ngoal  = color.RGBA{255, 150, 150, 255}
+	ygoal  = color.RGBA{65, 130, 35, 255}
+	player = color.RGBA{225, 227, 167, 255}
 
-	Red    = color.RGBA{255, 0, 0, 255}
-	Orange = color.RGBA{255, 125, 0, 255}
-	Yellow = color.RGBA{255, 225, 0, 255}
-	Green  = color.RGBA{55, 175, 20, 255}
-	Blue   = color.RGBA{20, 95, 175, 255}
-	Purple = color.RGBA{85, 20, 175, 255}
-	Brown  = color.RGBA{110, 60, 20, 255}
+	red    = color.RGBA{255, 0, 0, 255}
+	orange = color.RGBA{255, 125, 0, 255}
+	yellow = color.RGBA{255, 225, 0, 255}
+	green  = color.RGBA{55, 175, 20, 255}
+	blue   = color.RGBA{20, 95, 175, 255}
+	purple = color.RGBA{85, 20, 175, 255}
+	brown  = color.RGBA{110, 60, 20, 255}
 
-	PlayerPosition = map[string]int{
-		"x": 2, "y": 3,
-	}
-	Board = [Height][Width]int{
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-	}
-	_Box     = []Box{}
-	_Goal    = []Goal{}
-	_Steps   = [][]Step{}
-	TempStep = []Step{}
-	Color    = 0
-
-	Gameover = false
+	playerPoint = [2]int{2, 3}
+	board       = [height][width]int{}
+	box         = []Box{}
+	goal        = []Goal{}
+	steps       = [][]Step{}
+	step        = []Step{}
+	clr         = 0
 )
 
-type Game struct{}
+type Game struct {
+	clear bool
+}
+
+type Point struct {
+	x int
+	y int
+}
 
 type Box struct {
 	goal bool
-	x    int
-	y    int
+	Point
 }
 
 type Goal struct {
-	x int
-	y int
+	Point
 }
 
 type Step struct {
 	_type string
 	idx   int
-	x     int
-	y     int
-	goal  bool
+	Point
+	goal bool
 }
 
 func (g *Game) Update() error {
-	if Gameover {
+	if g.clear {
 		return nil
 	}
 
@@ -96,131 +91,125 @@ func (g *Game) Update() error {
 	}
 
 	if !ebiten.IsKeyPressed(ebiten.KeyU) && !ebiten.IsKeyPressed(ebiten.KeyR) && (directionX != 0 || directionY != 0) {
-		TempStep = append(TempStep, Step{_type: "player-move", x: PlayerPosition["x"], y: PlayerPosition["y"]})
+		step = append(step, Step{_type: "player-move"})
 	}
 
-	if inpututil.IsKeyJustPressed(ebiten.KeyU) && len(_Steps) > 0 {
-		step := _Steps[len(_Steps)-1]
+	if inpututil.IsKeyJustPressed(ebiten.KeyU) && len(steps) > 0 {
+		step := steps[len(steps)-1]
 
 		for _, st := range step {
 			switch st._type {
 			case "player-move":
-				PlayerPosition["x"] = st.x
-				PlayerPosition["y"] = st.y
+				playerPoint[0] = st.x
+				playerPoint[1] = st.y
 			case "box-move":
-				_Box[st.idx].x = st.x
-				_Box[st.idx].y = st.y
+				box[st.idx].x = st.x
+				box[st.idx].y = st.y
 			case "box-goal":
-				_Box[st.idx].setGoal(st.goal)
+				box[st.idx].setGoal(st.goal)
 			}
 		}
 
-		_Steps = _Steps[:len(_Steps)-1]
+		steps = steps[:len(steps)-1]
 	}
 
 	if inpututil.IsKeyJustPressed(ebiten.KeyR) {
-		PlayerPosition = map[string]int{
-			"x": 2, "y": 3,
-		}
-		_Steps = [][]Step{}
-		TempStep = []Step{}
-		Color = rand.Intn(7)
-		initialization()
+		reset()
 	}
 
-	PlayerPosition["x"] += directionX
-	PlayerPosition["y"] += directionY
+	playerPoint[0] += directionX
+	playerPoint[1] += directionY
 
-	for idx := 0; idx < len(_Box); idx++ {
-		newbox := BoxFilter(_Box, func(box Box) bool {
-			return box.x == _Box[idx].x+directionX && box.y == _Box[idx].y+directionY
+	for idx := 0; idx < len(box); idx++ {
+		newbox := BoxFilter(box, func(bx Box) bool {
+			return bx.x == box[idx].x+directionX && bx.y == box[idx].y+directionY
 		})
 
-		if _Box[idx].x == PlayerPosition["x"] && _Box[idx].y == PlayerPosition["y"] && len(newbox) == 0 {
-			TempStep = append(TempStep, Step{_type: "box-move", idx: idx, x: _Box[idx].x, y: _Box[idx].y})
-			_Box[idx].move(directionX, directionY)
+		if box[idx].x == playerPoint[0] && box[idx].y == playerPoint[1] && len(newbox) == 0 {
+			step = append(step, Step{_type: "box-move", idx: idx, Point: Point{x: box[idx].x, y: box[idx].y}})
+			box[idx].move(directionX, directionY)
 
-			if _Box[idx].x < 0 {
-				PlayerPosition["x"] -= directionX
-				_Box[idx].x = 0
+			if box[idx].x < 0 {
+				playerPoint[0] -= directionX
+				box[idx].x = 0
 			}
 
-			if _Box[idx].x > Width-1 {
-				PlayerPosition["x"] -= directionX
-				_Box[idx].x = Width - 1
+			if box[idx].x > width-1 {
+				playerPoint[0] -= directionX
+				box[idx].x = width - 1
 			}
 
-			if _Box[idx].y < 0 {
-				PlayerPosition["y"] -= directionY
-				_Box[idx].y = 0
+			if box[idx].y < 0 {
+				playerPoint[1] -= directionY
+				box[idx].y = 0
 			}
 
-			if _Box[idx].y > Height-1 {
-				PlayerPosition["y"] -= directionY
-				_Box[idx].y = Height - 1
+			if box[idx].y > height-1 {
+				playerPoint[1] -= directionY
+				box[idx].y = height - 1
 			}
 		}
 	}
 
-	colidbox := BoxFilter(_Box, func(box Box) bool {
-		return box.x == PlayerPosition["x"] && box.y == PlayerPosition["y"]
+	colidbox := BoxFilter(box, func(box Box) bool {
+		return box.x == playerPoint[0] && box.y == playerPoint[1]
 	})
 	if len(colidbox) != 0 {
-		PlayerPosition["x"] -= directionX
-		PlayerPosition["y"] -= directionY
+		playerPoint[0] -= directionX
+		playerPoint[1] -= directionY
 	}
 
-	if PlayerPosition["x"] < 0 {
-		PlayerPosition["x"] = 0
+	if playerPoint[0] < 0 {
+		playerPoint[0] = 0
 	}
-	if PlayerPosition["x"] > Width-1 {
-		PlayerPosition["x"] = Width - 1
+	if playerPoint[0] > width-1 {
+		playerPoint[0] = width - 1
 	}
-	if PlayerPosition["y"] < 0 {
-		PlayerPosition["y"] = 0
+	if playerPoint[1] < 0 {
+		playerPoint[1] = 0
 	}
-	if PlayerPosition["y"] > Height-1 {
-		PlayerPosition["y"] = Height - 1
+	if playerPoint[1] > height-1 {
+		playerPoint[1] = height - 1
 	}
 
 	cancelGoal()
-	Gameover = checkWin()
+	g.clear = checkWin()
 
-	if len(TempStep) > 0 {
-		_Steps = append(_Steps, TempStep)
+	if len(step) > 0 {
+		steps = append(steps, step)
 	}
-	TempStep = []Step{}
+	step = []Step{}
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	BoardClear()
-	screen.Fill(White)
-	for _, ga := range _Goal {
-		Board[ga.y][ga.x] = 2
+	board = [height][width]int{}
+	screen.Fill(white)
+	for _, ga := range goal {
+		board[ga.y][ga.x] = 2
 	}
 
-	for _, bx := range _Box {
+	for _, bx := range box {
 		if bx.goal {
-			Board[bx.y][bx.x] = 3
+			board[bx.y][bx.x] = 3
 		} else {
-			Board[bx.y][bx.x] = 1
+			board[bx.y][bx.x] = 1
 		}
 	}
 
-	for y := -1; y < Height+1; y++ {
-		for x := -1; x < Width+1; x++ {
+	for y := -1; y < height+1; y++ {
+		for x := -1; x < width+1; x++ {
 			_x := float64(50 * (x + 1))
 			_y := float64(50 * (y + 1))
 
-			if x == -1 || y == -1 || x == Width || y == Height {
+			if x == -1 || y == -1 || x == width || y == height {
 				// 테두리
-				ebitenutil.DrawRect(screen, _x, _y, 50, 50, getColor(Color))
-			} else if x == PlayerPosition["x"] && y == PlayerPosition["y"] {
+				ebitenutil.DrawRect(screen, _x, _y, 50, 50, colors[clr])
+			} else if x == playerPoint[0] && y == playerPoint[1] {
 				// 플레이어
-				ebitenutil.DrawRect(screen, _x, _y, 50, 50, Player)
+				ebitenutil.DrawRect(screen, _x, _y, 50, 50, player)
 			} else {
-				ebitenutil.DrawRect(screen, _x, _y, 50, 50, getBlock(Board[y][x], Color))
+				ebitenutil.DrawRect(screen, _x, _y, 50, 50, getBlock(board[y][x], clr))
 			}
 		}
 	}
@@ -228,7 +217,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	image, _, err := ebitenutil.NewImageFromFile("./assets/game-clear.png")
 	if err != nil {
 		log.Fatal(err)
-	} else if Gameover {
+	} else if g.clear {
 		sizeX := float64(image.Bounds().Size().X / 2)
 		sizeY := float64(image.Bounds().Size().Y / 2)
 
@@ -242,7 +231,7 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	}
 }
 
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+func (g *Game) Layout(outsidewidth, outsideheight int) (screenwidth, screenheight int) {
 	return screenSizeX, screenSizeY
 }
 
@@ -256,71 +245,50 @@ func (box *Box) setGoal(goal bool) {
 }
 
 func NewBox(point [2]int) {
-	_Box = append(_Box, Box{goal: false, x: point[0], y: point[1]})
+	box = append(box, Box{goal: false, Point: Point{x: point[0], y: point[1]}})
 }
 
 func NewGoal(point [2]int) {
-	_Goal = append(_Goal, Goal{x: point[0], y: point[1]})
+	goal = append(goal, Goal{Point{x: point[0], y: point[1]}})
 }
 
-func getColor(color int) color.RGBA {
-	switch color {
+func getBlock(block int, color int) color.RGBA {
+	switch block {
 	case 0:
-		return Red
+		return tile
 	case 1:
-		return Orange
+		return colors[color]
 	case 2:
-		return Yellow
+		return ngoal
 	case 3:
-		return Green
-	case 4:
-		return Blue
-	case 5:
-		return Purple
-	case 6:
-		return Brown
+		return ygoal
+	default:
+		return tile
 	}
-
-	return Tile
-}
-
-func getBlock(tile int, color int) color.RGBA {
-	switch tile {
-	case 0:
-		return Tile
-	case 1:
-		return getColor(color)
-	case 2:
-		return NGoal
-	case 3:
-		return YGoal
-	}
-
-	return Tile
 }
 
 func checkWin() bool {
 	var stack int
-	for idx := 0; idx < len(_Box); idx++ {
-		for _, goal := range _Goal {
-			if goal.x == _Box[idx].x && goal.y == _Box[idx].y {
+	for idx := 0; idx < len(box); idx++ {
+		for _, goal := range goal {
+			if goal.x == box[idx].x && goal.y == box[idx].y {
 				stack++
-				if len(TempStep) > 0 {
-					TempStep = append(TempStep, Step{_type: "box-goal", idx: idx, goal: _Box[idx].goal})
+				if len(step) > 0 {
+					step = append(step, Step{_type: "box-goal", idx: idx, goal: box[idx].goal})
 				}
-				_Box[idx].setGoal(true)
+				box[idx].setGoal(true)
 			}
 		}
 	}
 
-	return len(_Goal) == stack
+	return len(goal) == stack
 }
 
 func cancelGoal() {
-	for idx := 0; idx < len(_Box); idx++ {
-		for _, goal := range _Goal {
-			if _Box[idx].goal && !(goal.x == _Box[idx].x && goal.y == _Box[idx].y) {
-				_Box[idx].setGoal(false)
+	for idx := 0; idx < len(box); idx++ {
+		for _, goal := range goal {
+			if box[idx].goal && !(goal.x == box[idx].x && goal.y == box[idx].y) {
+				box[idx].setGoal(false)
 			}
 		}
 	}
@@ -336,19 +304,12 @@ func BoxFilter(vs []Box, f func(Box) bool) []Box {
 	return vsf
 }
 
-func BoardClear() {
-	Board = [Height][Width]int{
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-		{0, 0, 0, 0, 0, 0, 0, 0},
-	}
-}
+func reset() {
+	board = [height][width]int{}
+	box = []Box{}
+	goal = []Goal{}
 
-func initialization() {
-	_Box = []Box{}
-	_Goal = []Goal{}
+	playerPoint = [2]int{2, 3}
 
 	points := [...][2]int{{1, 3}, {3, 2}, {7, 4}, {4, 3}, {3, 1}, {9, 5}, {12, 10}, {3, 11}, {14, 4}, {3, 3}, {1, 10}, {10, 2}, {10, 6}, {7, 10}}
 	for _, point := range points {
@@ -359,13 +320,17 @@ func initialization() {
 	for _, point := range points {
 		NewGoal(point)
 	}
+
+	steps = [][]Step{}
+	step = []Step{}
+	clr = rand.Intn(7)
 }
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
-	Color = rand.Intn(7)
+	clr = rand.Intn(7)
 
-	initialization()
+	reset()
 }
 
 func main() {
